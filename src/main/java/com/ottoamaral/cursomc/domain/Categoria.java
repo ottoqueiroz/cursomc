606fa1614cd9a93ef2d5b1297a4f99ae99a3d734
Otto Amaral <ottoamaraldeveloper@gmail.com>
2020-01-05 00:01:49 -0300

    Banco de dados H2 e criacao automatica da base de dados

--- src/main/java/com/ottoamaral/cursomc/domain/Categoria.java
+++ src/main/java/com/ottoamaral/cursomc/domain/Categoria.java
@@ -2,10 +2,18 @@ package com.ottoamaral.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = -2946352634675069124L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 
