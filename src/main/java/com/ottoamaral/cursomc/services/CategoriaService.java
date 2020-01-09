80ae7b3b0aac2304f8884e72665410496ea5429f
Otto Amaral <ottoamaraldeveloper@gmail.com>
2020-01-09 00:29:04 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/ottoamaral/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.ottoamaral.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ottoamaral.cursomc.domain.Categoria;
+import com.ottoamaral.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
