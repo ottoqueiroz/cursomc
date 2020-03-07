077ed2ac897df146573877ce64c2e9a7589188b2
Otto Amaral <ottoamaraldeveloper@gmail.com>
2020-03-07 17:20:16 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/ottoamaral/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.ottoamaral.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ottoamaral.cursomc.domain.Cliente;
+import com.ottoamaral.cursomc.repositories.ClienteRepository;
+import com.ottoamaral.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente find(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
