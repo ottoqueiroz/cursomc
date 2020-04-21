f9249838a6fee248ea89b7777d01add11fda5bb4
Otto Amaral <ottoamaraldeveloper@gmail.com>
2020-04-21 15:58:06 -0300

    ItemPedido and ItemPedidoPK

--- src/main/java/com/ottoamaral/cursomc/domain/Pedido.java
+++ src/main/java/com/ottoamaral/cursomc/domain/Pedido.java
@@ -2,6 +2,8 @@ package com.ottoamaral.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -24,14 +27,17 @@ public class Pedido implements Serializable {
 
 	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
 	private Pagamento pagamento;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
+
+	@OneToMany(mappedBy = "id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
 
 	public Pedido() {
 
@@ -83,6 +89,14 @@ public class Pedido implements Serializable {
 
 	public void setEndereco(Endereco enderecoDeEntrega) {
 		this.enderecoDeEntrega = enderecoDeEntrega;
+	}
+
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
 	}
 
 	@Override
