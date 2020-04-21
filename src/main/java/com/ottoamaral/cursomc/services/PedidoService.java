60fc84796219ed0c3bc403c09ce08f9983a3a5c0
Otto Amaral <ottoamaraldeveloper@gmail.com>
2020-04-21 16:49:13 -0300

    Endpoint /pedidos{id} disponibilizado

---
+++ src/main/java/com/ottoamaral/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.ottoamaral.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ottoamaral.cursomc.domain.Pedido;
+import com.ottoamaral.cursomc.repositories.PedidoRepository;
+import com.ottoamaral.cursomc.services.exception.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+
+	public Pedido find(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+}
